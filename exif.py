1cec184890e7c27b19380b0ea3a02fd5a8e34513
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2024-05-26 12:58:15 +0200

    EXIF Renamer

---
+++ exif.py
@@ -0,0 +1,64 @@
+import os
+import re
+from pathlib import Path
+from typing import Optional
+
+import exifread
+
+
+class PhotoTimestampRenamer:
+    DIR_PATH = r"D:\Fotos\neu + unsortiert\B Merge notwendig\Weiberfastnacht (12.02.2015)\Lena"
+
+    def get_exif_data(self, image_path) -> Optional[str]:
+        # Öffne das Bild und lese die EXIF-Daten
+        with open(image_path, 'rb') as image_file:
+            tags = exifread.process_file(image_file)
+
+        # Hol das Aufnahmedatum aus den EXIF-Daten
+        date_taken = tags.get('EXIF DateTimeOriginal')
+
+        if date_taken:
+            return str(date_taken)
+        else:
+            return None
+
+    def process(self):
+        failure_counter = 0
+        rename_counter = 0
+
+        for element in os.scandir(Path(self.DIR_PATH)):
+            if not element.is_file():
+                continue
+            extension = Path(element).suffix.lower()
+            if extension not in (".jpg", ".jpeg"):
+                continue
+
+            date_taken = self.get_exif_data(element)
+            if date_taken is None:
+                continue
+
+            pattern = r"^(\d{4}):(\d{2}):(\d{2})\s(\d{2}):(\d{2}):(\d{2})$"
+            matches = re.search(pattern, date_taken)
+            if not matches:
+                continue
+
+            year = matches[1]
+            month = matches[2]
+            day = matches[3]
+            hour = matches[4]
+            minute = matches[5]
+            second = matches[6]
+
+            new_filename = f"{year}-{month}-{day} {hour}.{minute}.{second}{extension}"
+            try:
+                os.rename(element.path, element.path.replace(element.name, '') + new_filename)
+                failure_counter = 0
+            except FileExistsError:
+                new_filename = f"{year}-{month}-{day} {hour}.{minute}.{second}-{failure_counter + 1}{extension}"
+                os.rename(element.path, element.path.replace(element.name, '') + new_filename)
+            rename_counter += 1
+
+        print(f"{rename_counter} files renamed.")
+
+if __name__ == "__main__":
+    PhotoTimestampRenamer().process()
--- renamer.py
+++ renamer.py
@@ -4,28 +4,37 @@ from pathlib import Path
 
 
 class Renamer:
-    FOTO_DIR = "D:\\Fotos\\neu + unsortiert\\A Sortierbar\\Kölnpfad mit Jessi 2021\\11 Etappe 08"
+    FOTO_DIR = r"D:\Fotos\neu + unsortiert\B Merge notwendig\Hochzeit Luise & Otti (7.9.17)\Luise"
 
     PATTERN_OLD_SAMSUNG = r"^(\d{4})(\d{2})(\d{2})[_\-](\d{2})(\d{2})(\d{2})%s$"
+    PATTERN_GOOGLE = r"^PXL_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})\d{3}.*?%s$"
     PATTERN_WHATSAPP = r"^IMG-(\d{4})(\d{2})(\d{2})-W\d+%s$"
+    PATTERN_ALEJANDRO = r"IMG_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})%s$"
+    PATTERN_FOTO = r"Foto\s(\d{2}).(\d{2}).(\d{2}),\s(\d{2})\s(\d{2})\s(\d{2})[\w\(\))]*%s$"
 
     def process(self):
         rename_counter = 0
+        failure_counter = 0
         for element in os.scandir(Path(self.FOTO_DIR)):
             if element.is_file():
                 extension = Path(element).suffix.lower()
                 filename = element.name
-                matches = re.search(self.PATTERN_OLD_SAMSUNG % extension, filename)
+                matches = re.search(self.PATTERN_FOTO % extension, filename)
                 if not matches:
                     continue
-                year = matches[1]
+                year = matches[1] if len(matches[1]) == 4 else f"20{matches[1]}"
                 month = matches[2]
                 day = matches[3]
                 hour = matches[4]
                 minute = matches[5]
                 second = matches[6]
-                new_filename = f"{year}-{month}-{day} {hour}.{minute}.{second}.jpg"
-                os.rename(element.path, element.path.replace(filename, '') + new_filename)
+                new_filename = f"{year}-{month}-{day} {hour}.{minute}.{second}{extension}"
+                try:
+                    os.rename(element.path, element.path.replace(filename, '') + new_filename)
+                    failure_counter = 0
+                except FileExistsError:
+                    new_filename = f"{year}-{month}-{day} {hour}.{minute}.{second}-{failure_counter + 1}{extension}"
+                    os.rename(element.path, element.path.replace(filename, '') + new_filename)
                 rename_counter += 1
 
         print(f"{rename_counter} files renamed.")
